7ddb1bf4d064399f84942dbb6ff78b3d9fb69c76
SageSir <kweku.ammuako.annan@gmail.com>
2022-10-13 18:23:45 -0700

    Correction for 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,22 +1,16 @@
 #include <stdio.h>
 
 /**
- *
- * Main - Prints: The size of various types of data
- *
+ * main - Prints the size of various types of data
+ * Description: 'C program that checks size of data types'
  * Return: Always 0 (Successful)
  */
 int main(void)
 {
-	char a;
-	int b;
-	long int c;
-	long long int d;
-	float e;
-
-	printf("Size of a char: %d byte(s)\n", sizeof(a));
-	printf("Size of an int: %d byte(s)\n", sizeof(b));
-	printf("Size of a long int: %dbyte(s)\n", sizeof(c));
-	printf("Size of a float: %d byte(s)\n", sizeof(e));
-	return(0);
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
+	return (0);
 }
