8dfa49b3efb574465534b6b5bb0222251e61f19e
SageSir <kweku.ammuako.annan@gmail.com>
2022-10-14 20:24:13 -0700

    Correction for 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,22 +1,20 @@
 #include <stdio.h>
 
 /**
- *
- *  * main - prints the alphabet in lowercase,
- *   * followed by a new line
- *    * Return: Always 0 (Success)
- *     */
+ * main - prints the alphabet in lowercase,
+ * followed by a new line
+ * Description: 'C program which prints alphabet in lowercase'
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
+	char ch;
 
-		char ch;
-
-
-	        for (ch = 'a'; ch <= 'z'; ch++)
-		{
-			putchar(ch);
-		}
-		putchar('\n');
-		return (0);
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
 }
